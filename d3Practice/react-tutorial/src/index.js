eb9e4ff5cdf3d8188bc840387952751c9b41d917
Zander Meitus <zmeitus@deloitte.com>
2021-03-04 20:02:32 -0500

    add check for winner

--- d3Practice/react-tutorial/src/index.js
+++ d3Practice/react-tutorial/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Square is no longer a component class, it is a component function. no state, no props of its own
 function Square(props) {
   return (
     <button
@@ -25,6 +26,9 @@ class Board extends React.Component {
 
    handleClick(i) {
     const squares = this.state.squares.slice(); // create  copy of state - immutability - instead of mutating state
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -43,11 +47,16 @@ class Board extends React.Component {
     );
   }
 
-
-
   // render some HTML with 9 Squares
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
 
     return (
       <div>
@@ -96,3 +105,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  // winning combinations
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === sqaures[c]) {
+      return squares[a]
+    }
+  }
+  return null;
+}
